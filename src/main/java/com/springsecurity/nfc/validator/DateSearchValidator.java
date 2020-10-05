ec45fe74915004cff71cea873a0490739a1cfee5
krati13 <30385753+krati13@users.noreply.github.com>
2020-10-05 16:22:10 +0530

    Update DateSearchValidator.java

--- src/main/java/com/springsecurity/nfc/validator/DateSearchValidator.java
+++ src/main/java/com/springsecurity/nfc/validator/DateSearchValidator.java
@@ -18,10 +18,10 @@ public class DateSearchValidator implements Validator, Constants {
 	public void validate(Object target, Errors errors) {
 		DateSearch ds = (DateSearch) target;
 		if (ds.getCreated_from()==null || ds.getCreated_from().equalsIgnoreCase(EMPTY_STRING))
-			errors.rejectValue(CREATED_FROM, "NULL", "Validation Error : Create From date is empty");
+			errors.rejectValue(CREATED_FROM, "NULL", "Validation Error :: Create From date is empty");
 		
 		if (ds.getCreated_to()==null || ds.getCreated_to().equalsIgnoreCase(EMPTY_STRING))
-			errors.rejectValue(CREATED_TO, "NULL", "Validation Error : Create To date is empty");
+			errors.rejectValue(CREATED_TO, "NULL", "Validation Error :: Create To date is empty");
 	}
 	
 }
